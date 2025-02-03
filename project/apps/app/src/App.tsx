66fc381651a49a48008d8d6b45aa0dad13d4606f
purellarohith <purellarohith@gmail.com>
2025-02-04 04:44:53 +0530

    completed task-2

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -28,24 +28,7 @@ const App = () => {
   return (
   <>
     <h1>Pokemon list:</h1>
-    {/* <List data={pokemonList} /> */}
-    <div id='container'>
-        Question 1: How did you manage to fetch the list and what tool did you use?
-        <br />
-        Ans: I used the Axios package for fetching data instead of the native fetch API.
-        <br />
-        <br />
-        <br />
-        Question 2: What steps would you take to future improve this?
-        <br />
-        Ans: Here are some improvements i can make to enhance code readability..
-        <br />
-        1) I will implement redux toolkit for managing store
-        <br />
-        2) I will create a pokimon slice in redux toolkit and make all the api calls and state management related stuff in this slice.
-        <br />
-        3) I will store the fetched data in redux store for better state management.
-    </div>
+    <List data={pokemonList} />
   </>
   )
 }
