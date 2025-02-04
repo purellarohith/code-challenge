8b323b880996921feeb0a07d68fcb74477bf42be
purellarohith <purellarohith@gmail.com>
2025-02-04 06:29:37 +0530

    completed task-4

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -32,7 +32,17 @@ const App = () => {
   <>
     <h1>Pokemon list:</h1>
     <List data={pokemonList} />
-    
+    <div id='container'>
+        Question 4: What makes the createSlice in redux-toolkit difference then A Reducer in redux?        
+        <br />
+        Ans: createSlice has combining action creators and reducers in one function.Underneth redux toolkit uses Immer for immutable updates. We don't need to use switch statements anymore.
+        <br />
+        <br />
+        <br />
+        Question 5: Describe the benefits of immutable code.
+        <br />
+        Ans:The Immutable code prevents accidental mutations, making debugging easier and ensuring predictable state updates.
+    </div>
   </>
   )
 }
