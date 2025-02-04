0327ea7acc2acadc80157fca74154e159054d2f0
purellarohith <purellarohith@gmail.com>
2025-02-04 10:34:44 +0530

    completed task-5

--- project/packages/ui/components/List.tsx
+++ project/packages/ui/components/List.tsx
@@ -1,5 +1,42 @@
 import React from 'react'
+import { useDispatch } from "react-redux";
+import { removePokemon } from '../../../apps/app/src/redux/slices/pokemonSlice';
 
-export const List = () => {
-  return <p>List</p>
+
+type Pokemon = {
+  name: string;
+  url: string;
+};
+
+type PokemonList ={
+  data:Pokemon[];
 }
+
+
+export const List:React.FC<PokemonList> = ({
+  data
+}:PokemonList) => {
+
+  const dispatch = useDispatch();
+
+
+  const handleRemovePokemon = (name: string) => {
+    dispatch(removePokemon({name}));
+  };
+
+  return (
+  <div>
+    <div id='list'>
+      {
+        data.map((val,index) => (
+          <div key={val.name} className='itemContainer'>
+          <label >{`${index + 1}) ${val.name}`}</label>
+          <button  onClick={() => handleRemovePokemon(val.name)}>X</button>
+          </div>
+        ))
+      }
+    </div>
+  </div>)
+}
+
+
